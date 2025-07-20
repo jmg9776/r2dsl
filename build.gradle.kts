b185bd3c2157a1045888c04bda419b8901e88c32
조 <jmg97@kakao.com>
2025-07-20 19:28:02 +0900

    feat: configure Maven Central publishing with vanniktech plugin - apply vanniktech.maven.publish plugin (v0.34.0) - add POM metadata (name, license, developers, scm, etc.) - set up GPG signing and Maven Central publication - ensure plugin resolution via pluginManagement block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 group = "io.github.jmg9776"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -16,6 +17,46 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.jmg9776",
+        artifactId = "r2dsl",
+        version = "1.0.0"
+    )
+
+    pom {
+        name.set("r2dsl-core")
+        description.set("DSL and lightweight ORM for R2DBC written in Kotlin.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/jmg9776/r2dsl")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("jmg9776")
+                name.set("Min-Gyun Cho")
+                email.set("jmg97@kakao.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/jmg9776/r2dsl.git")
+            developerConnection.set("scm:git:ssh://github.com/jmg9776/r2dsl.git")
+            url.set("https://github.com/jmg9776/r2dsl")
+        }
+    }
+
+    publishToMavenCentral()
+    signAllPublications()
 }
